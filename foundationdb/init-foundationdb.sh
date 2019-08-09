751913ba34505918acd473b3afcf676d3acffd8e
Anton Leonov <jeanleonov@gmail.com>
2019-08-09 17:27:34 +0300

    Use appscale cache directory for packages cache

--- foundationdb/init-foundationdb.sh
+++ foundationdb/init-foundationdb.sh
@@ -9,7 +9,7 @@ set -eu
 ### Helper functions ###
 ########################
 
-APT_CACHE='/var/cache/apt/archives'
+PACKAGE_CACHE='/var/cache/appscale'
 PACKAGE_MIRROR='http://s3.amazonaws.com/appscale-build'
 
 log() {
@@ -18,10 +18,10 @@ log() {
 }
 
 cachepackage() {
-    cached_file="${APT_CACHE}/$1"
+    cached_file="${PACKAGE_CACHE}/$1"
     remote_file="${PACKAGE_MIRROR}/$1"
     expected_md5="$2"
-    mkdir -p ${APT_CACHE}
+    mkdir -p ${PACKAGE_CACHE}
     if [ -f ${cached_file} ]; then
         md5=($(md5sum ${cached_file}))
         if [ "$md5" = "$2" ]; then
@@ -138,27 +138,29 @@ fi
 
 SCRIPT_DIR="$( realpath --strip "$( dirname "${BASH_SOURCE[0]}" )" )"
 
+mkdir -p "${PACKAGE_CACHE}"
+
 
 ### Installing FDB clients package ###
 #------------------------------------#
 FDB_CLIENTS_PKG='foundationdb-clients_6.1.8-1_amd64.deb'
 FDB_CLIENTS_MD5='f701c23c144cdee2a2bf68647f0e108e'
-log "Making sure ${FDB_CLIENTS_PKG} is in ${APT_CACHE}"
+log "Making sure ${FDB_CLIENTS_PKG} is in ${PACKAGE_CACHE}"
 cachepackage "${FDB_CLIENTS_PKG}" "${FDB_CLIENTS_MD5}"
 
 log "Installing ${FDB_CLIENTS_PKG}"
-dpkg --install ${APT_CACHE}/foundationdb-clients_6.1.8-1_amd64.deb
+dpkg --install ${PACKAGE_CACHE}/foundationdb-clients_6.1.8-1_amd64.deb
 
 
 ### Installing FDB server package ###
 #-----------------------------------#
 FDB_SERVER_PKG='foundationdb-server_6.1.8-1_amd64.deb'
 FDB_SERVER_MD5='80a427be14a329d864a91c9ce464d73c'
-log "Making sure ${FDB_SERVER_PKG} is in ${APT_CACHE}"
+log "Making sure ${FDB_SERVER_PKG} is in ${PACKAGE_CACHE}"
 cachepackage "${FDB_SERVER_PKG}" "${FDB_SERVER_MD5}"
 
 log "Installing ${FDB_SERVER_PKG}"
-dpkg --install ${APT_CACHE}/foundationdb-server_6.1.8-1_amd64.deb
+dpkg --install ${PACKAGE_CACHE}/foundationdb-server_6.1.8-1_amd64.deb
 
 
 ### Ensuring FDB directories are accessible ###
