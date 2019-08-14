814c6cc46b4380e57b48ced372874f241283d42e
Anton Leonov <jeanleonov@gmail.com>
2019-08-14 17:35:43 +0300

    Make sh files executable for owner

--- download_all_artifacts.sh
+++ download_all_artifacts.sh
@@ -2,6 +2,8 @@
 # This script is used by appscale bootstrap.sh in order to prepare
 # all needed artifacts in appscale image.
 
-for script in $(find ./ -name download_artifacts.sh) ; do
+SCRIPT_DIR="$( realpath --strip "$( dirname "${BASH_SOURCE[0]}" )" )"
+
+for script in $(find "${SCRIPT_DIR}" -name download_artifacts.sh) ; do
   bash ${script}
 done
