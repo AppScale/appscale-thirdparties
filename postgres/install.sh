d110fa82a3b66c002d91bb6ce33ac346ab3cd1f9
Anton Leonov <jeanleonov@gmail.com>
2019-09-23 18:48:05 +0300

    Run apt-get update before installing postgres

--- postgres/install.sh
+++ postgres/install.sh
@@ -4,6 +4,19 @@
 SCRIPT_DIR="$( realpath --strip "$( dirname "${BASH_SOURCE[0]}" )" )"
 source "$(dirname ${SCRIPT_DIR})/common.sh"
 
+log "Updating packages information"
+attempt=1
+while ! apt-get update
+do
+    if (( attempt > 15 )); then
+        log "Failed to update packages information after ${attempt} attempts" "ERROR"
+        exit 1
+    fi
+    log "Failed to update packages information. Retrying." "WARNING"
+    ((attempt++))
+    sleep ${attempt}
+done
+
 log "Installing Postgres"
 attempt=1
 while ! apt-get --assume-yes install postgresql
